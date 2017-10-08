e6c2152f8599707b5bda8bd8ea03f9e573d767f2
derrick <kang.derrick@gmail.com>
2017-10-08 15:25:42 +0900

    added basic

---
+++ SummerMediaFileCell/SummerMediaFileCell.h
@@ -0,0 +1,19 @@
+//
+//  SummerMediaFileCell.h
+//  SummerMediaFileCell
+//
+//  Created by Kang Jinyeoung on 08/10/2017.
+//  Copyright © 2017 SuperbDerrick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SummerMediaFileCell.
+FOUNDATION_EXPORT double SummerMediaFileCellVersionNumber;
+
+//! Project version string for SummerMediaFileCell.
+FOUNDATION_EXPORT const unsigned char SummerMediaFileCellVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SummerMediaFileCell/PublicHeader.h>
+
+
